eb3ea49595b58225be5b91d8de49514dff6db55d
Rajan-sharma-0 <nk800109@gmail.com>
2025-07-01 22:49:56 +0530

    1/7

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express"
 const app = express()
 const {dirname: __dirname} = import.meta;
+import path from "path"
+import fs from "fs"
 
 app.set("view engine", 'ejs')
 app.use(express.json())
@@ -8,7 +10,23 @@ app.use(express.urlencoded({extended:true}))
 app.set(express.static(path.join(__dirname, "public")))
 
 app.get("/", function(req, res){
-    res.render("index")
+    fs.readdir(`./Files`, function(err, files){
+        res.render("index", {files: files})
+    })
 })
 
-app.listen(3003)
+app.get("/files/:filename", function(req, res){
+   fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
+    res.render('show', {filename: req.params.filename, filedata: filedata})
+   } )
+})
+
+
+app.post("/create", function(req, res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+        res.redirect("/")
+    })    
+    })
+
+
+app.listen(3000)
